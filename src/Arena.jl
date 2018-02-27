4ccc756f1c37f55135e8c12ff758b90b7c4ca263
dehann <dehann.fourie@gmail.com>
2018-02-27 10:53:59 -0500

    initial commit

---
+++ src/Arena.jl
@@ -0,0 +1,71 @@
+module Arena
+
+using
+  DrakeVisualizer,
+  Graphs,
+  RoMEPlotting,
+  KernelDensityEstimatePlotting,
+  TransformUtils,
+  CoordinateTransformations,
+  GeometryTypes,
+  ColorTypes,
+  MeshIO,
+  ImageMagick,
+  ImageView,
+  Images
+
+export
+  drawdbdirector,
+  drawPoses,
+  drawPosesLandm,
+  #drawsubmap
+  plot,
+  plotKDE,
+  meshgrid,
+  DepthCamera,
+  buildmesh!,
+  reconstruct,
+  VisualizationContainer,
+  startdefaultvisualization,
+  newtriad!,
+  visualize!,
+  visualizetriads,
+  visualizeallposes!,
+  visualizeDensityMesh!,
+  updaterealtime!,
+  visualizerealtime,
+  # new tree interface
+  drawpose!,
+  drawposepoints!,
+  drawLine!,
+  drawLineBetween!,
+  drawAllOdometryEdges!,
+  pointToColor,
+  findAllBinaryFactors,
+  drawAllBinaryFactorEdges!,
+  loadmodel,
+  DrawModel,
+  DrawROV,
+  DrawScene,
+  #deleting functions
+  deletemeshes!,
+
+  # more drawing utils
+  ArcPointsRangeSolve,
+  findaxiscenter!,
+  parameterizeArcAffineMap,
+  animatearc
+
+
+function visualize!(fg::FactorGraph, kawgs...)
+  error("visualize!(fg::FactorGraph, ...) not implemented yet")
+end
+
+include("VisualizationUtils.jl")
+include("ModelVisualizationUtils.jl")
+include("DBVisualizationUtils.jl")
+include("DirectorVisService.jl")
+
+
+
+end
