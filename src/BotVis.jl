5ae2f73973684c7f7177422457c1b9bb1d8de6b2
Johannes Terblanche <terblanche.j@gmail.com>
2018-12-14 15:05:13 +0200

    point cloud and cameramatrix

--- src/BotVis.jl
+++ src/BotVis.jl
@@ -4,6 +4,7 @@ const greenMat = MeshPhongMaterial(color=RGBA(0, 1, 0, 0.5))
 const redMat = MeshPhongMaterial(color=RGBA(0, 1, 0, 0.5))
 
 """
+	$(TYPEDEF)
 Type for 2d visualization
 """
 struct BotVis2
@@ -12,10 +13,50 @@ struct BotVis2
     landmarks::Dict{Symbol, NTuple{3,Float64}}
 end
 
-
 # something like this later for botvis 3
 # poses3::Dict{Symbol,Tuple{Point{3,Float32},Quat{Float64}}}
 
+#TODO: create a package that defines a camera model, there are lots of different ones all over the place
+"""
+	$(TYPEDEF)
+Data structure for a Camera model with parameters.
+Use `CameraModel(width,height,fc,cc,skew,kc)` for easy construction.
+"""
+struct CameraModel
+    width::Int		# image width
+    height::Int		# image height
+    fc::Vector{Float64}	# focal length in x and y
+	f::Float64			# if one focal lenght is used
+    cc::Vector{Float64}	# camera center
+    skew::Float64	    # skew value
+    kc::Vector{Float64} # distortion coefficients up to fifth order or NTuple{5,Float64}
+    K::Matrix{Float64} # 3x3 camera calibration matrix (Camera intrinsics)
+    Ki::Matrix{Float64} # inverse of a 3x3 camera calibratio matrix
+end
+
+"""
+	$(SIGNATURES)
+Constructor helper for creating a camera model.
+"""
+function CameraModel(width::Int,height::Int,fc::Vector{Float64},cc::Vector{Float64},skew::Float64,kc::Vector{Float64})
+    KK = [fc[1]      skew  cc[1];
+             0       fc[2] cc[2];
+             0		    0     1]
+    Ki = inv(KK)
+    CameraModel(width,height,fc, mean(fc),cc,skew,kc,KK,Ki)
+end
+
+"""
+	$(SIGNATURES)
+Constructor helper for creating a camera model.
+"""
+function CameraModel(width::Int,height::Int,f::Float64,cc::Vector{Float64})
+    KK = [f 0 cc[1];
+          0 f cc[2];
+          0	0     1.]
+    Ki = inv(KK)
+    CameraModel(width,height,[f,f], f, cc, 0.0, [0.], KK,Ki)
+end
 
 
 """
@@ -34,7 +75,7 @@ end
     $(SIGNATURES)
 Draw all poses in an 2d factor graph, use meanmax = :max or :mean for distribution max or mean, respectively.
 """
-function drawPoses2!(botvis::BotVis2, fgl::FactorGraph; meanmax::Symbol=:max, triadLength=1.0)
+function drawPoses2!(botvis::BotVis2, fgl::FactorGraph; meanmax::Symbol=:max, triadLength=0.25)
 
     xx, ll = ls(fgl)
 
@@ -82,21 +123,25 @@ end
 #TODO its a start, still need transform etc.
 """
     $(SIGNATURES)
-Create a point cloud from a depth image.
+Create a {Float32} point cloud from a depth image. Note: rotated to Forward Starboard Down
 """ #TODO fix color map
-function cloudFromDepthImage(depths::Array{UInt16,2}, #=colmap::Vector{RGB{N0f8}} = repeatedColorMap=#; d::Int = 2,
-                            cu::Float32 = 322.042f0, cv::Float32 = 238.544f0, f::Float32 = 387.205f0, maxrange::Float32 = 10f0)
+function cloudFromDepthImage(depths::Array{UInt16,2}, cm::CameraModel#=colmap::Vector{RGB{N0f8}} = repeatedColorMap=#;
+							 depthscale = 0.001f0, skip::Int = 2, maxrange::Float32 = 5f0)
 
+	cx = Float32(cm.cc[1])
+	cy = Float32(cm.cc[2])
+	fx = Float32(cm.fc[1])
+	fy = Float32(cm.fc[2])
     (row,col) = size(depths)
     cloud = Point3f0[]
     cloudCol = RGB{Float32}[]
 
-    for u = 1:d:row, v = 1:d:col
-        z = depths[u,v]*0.001f0
+    for u = 1:skip:row, v = 1:skip:col
+        z = depths[u,v]*depthscale
         if  0 < z < maxrange
-            x = (u-cu)/f * z
-            y = (v-cv)/f * z
-            push!(cloud, Point3f0(z,x,y))
+            x = (v-cx)/fx * z
+			y = (u-cy)/fy * z
+            push!(cloud, Point3f0(z,x,y)) #NOTE rotated to Forward Starboard Down, TODO: maybe leave in camera frame?
             push!(cloudCol, RGB{Float32}(0.,1.,0.))#colmap[round(Int,(z/maxrange)*2559f0)+1]
         end
     end
@@ -105,20 +150,16 @@ function cloudFromDepthImage(depths::Array{UInt16,2}, #=colmap::Vector{RGB{N0f8}
     return pointcloud
 end
 
+
 """
     $(SIGNATURES)
 Draw point cloud on pose.
-"""
-function drawPointCloudonPose!(botvis::BotVis2, x::Symbol, pointcloud::PointCloud)
+xTc -> pose to camera transform
+""" #TODO: confirm xTc or cTx
+function drawPointCloudonPose!(botvis::BotVis2, x::Symbol, pointcloud::PointCloud, xTc::SE3 = SE3([0,0,0],I))
 
     setobject!(botvis.vis[:poses][x][:pc], pointcloud)
+	trans = Translation(xTc.t[1], xTc.t[2], xTc.t[3])∘LinearMap(Quat(xTc.R.R))
+	settransform!(botvis.vis[:poses][x][:pc], trans)
 
 end
-
-
-##
-# """
-# botvis = initBotVis2(showLocal=true)
-# drawPoses!(botvis, fg)
-# drawLandmarks2!(botvis, fg)
-# """"
