9fe9b28a0feaed9dfd1faf99dd0b0f593dbf805c
dehann <dehann.fourie@gmail.com>
2019-01-17 08:30:42 -0500

    botvis not deprecated

--- src/BotVis.jl
+++ src/BotVis.jl
@@ -15,3 +15,60 @@ function initBotVis2(;showLocal::Bool = true)::BotVis2
     showLocal && open(vis)
     return BotVis2(vis, Dict{Symbol, NTuple{3,Float64}}(), Dict{Symbol, NTuple{3,Float64}}())
 end
+
+
+"""
+    $(SIGNATURES)
+Draw all poses in an 2d factor graph, use meanmax = :max or :mean for distribution max or mean, respectively.
+"""
+function drawPoses2!(botvis::BotVis2,
+                     fgl::FactorGraph;
+                     meanmax::Symbol=:max,
+                     triadLength=0.25  )::Nothing
+    #
+    xx, ll = ls(fgl)
+
+    for x in xx
+        X = getKDE(fgl, x)
+        xmx = meanmax == :max ? getKDEMax(X) : getKDEMean(X)
+        if !haskey(botvis.poses, x)
+            triad = Triad(triadLength)
+            setobject!(botvis.vis[:poses][x], triad)
+            push!(botvis.poses, x => (xmx[1],xmx[2],xmx[3]))
+            trans = Translation(xmx[1:2]..., 0.0) ∘ LinearMap(RotZ(xmx[3]))
+            settransform!(botvis.vis[:poses][x], trans)
+        else
+            botvis.poses[x] => (xmx[1],xmx[2],xmx[3])
+            trans = Translation(xmx[1:2]..., 0.0) ∘ LinearMap(RotZ(xmx[3]))
+            settransform!(botvis.vis[:poses][x], trans)
+        end
+    end
+	return nothing
+end
+
+"""
+    $(SIGNATURES)
+Draw all landmarks in an 2d factor graph, use meanmax = :max or :mean for distribution max or mean, respectively.
+"""
+function drawLandmarks2!(botvis::BotVis2,
+                         fgl::FactorGraph;
+                         meanmax::Symbol=:max  )::Nothing
+    #
+    xx, ll = ls(fgl)
+
+    for x in ll
+        X = getKDE(fgl, x)
+        xmx = meanmax == :max ? getKDEMax(X) : getKDEMean(X)
+        if !haskey(botvis.landmarks, x)
+            setobject!(botvis.vis[:landmarks][x], lmpoint,  greenMat)
+            push!(botvis.landmarks, x => (xmx[1],xmx[2],0.))
+            trans = Translation(xmx[1:2]..., 0.0)
+            settransform!(botvis.vis[:landmarks][x], trans)
+        else
+            botvis.landmarks[x] => (xmx[1],xmx[2],0.0)
+            trans = Translation(xmx[1:2]..., 0.0)
+            settransform!(botvis.vis[:landmarks][x], trans)
+        end
+    end
+	return nothing
+end
