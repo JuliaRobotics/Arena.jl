558cc2cf2194392bdc470fe7fc207c92bfde2d29
Johannes Terblanche <terblanche.j@gmail.com>
2019-01-22 17:38:33 +0200

    add global transform and Caesar.ls

--- src/BotVis.jl
+++ src/BotVis.jl
@@ -30,7 +30,7 @@ function drawPoses2!(botvis::BotVis2,
                      triadLength=0.25,
                      sessionId::String="Session" )::Nothing
     #
-    xx, ll = ls(fgl)
+    xx, ll = Caesar.ls(fgl)
 
     for x in xx
         X = getKDE(fgl, x)
@@ -43,7 +43,7 @@ function drawPoses2!(botvis::BotVis2,
         else
             botvis.cachevars[x][3][:] = xmx
         end
-        settransform!(botvis.vis[Symbol(sessionId)][:poses][x], trans)
+        settransform!(botvis.vis[Symbol(sessionId)][:poses][x], drawtransform ∘ trans)
     end
 	return nothing
 end
@@ -57,19 +57,19 @@ function drawLandmarks2!(botvis::BotVis2,
                          meanmax::Symbol=:max,
                          sessionId::String="Session"  )::Nothing
     #
-    xx, ll = ls(fgl)
+    xx, ll = Caesar.ls(fgl)
 
     for x in ll
         X = getKDE(fgl, x)
         xmx = meanmax == :max ? getKDEMax(X) : getKDEMean(X)
         trans = Translation(xmx[1:2]..., 0.0)
-        if !haskey(botvis.landmarks, x)
+        if !haskey(botvis.cachevars, x)
             setobject!(botvis.vis[Symbol(sessionId)][:landmarks][x], lmpoint, greenMat)
             botvis.cachevars[x] = (:Point2, [true;], [xmx[1]; xmx[2]; 0.0 ] )
         else
             botvis.cachevars[x][3][1:2] = xmx[1:2]
         end
-        settransform!(botvis.vis[Symbol(sessionId)][:landmarks][x], trans)
+        settransform!(botvis.vis[Symbol(sessionId)][:landmarks][x], drawtransform ∘ trans)
     end
 	return nothing
 end
