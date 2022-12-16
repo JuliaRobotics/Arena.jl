faebb5af2665b7a685f2eea7a85a3432ad5fac3d
dehann <dehann.fourie@gmail.com>
2022-12-15 17:51:50 -0800

    plotting fixes

--- src/services/PlotPointCloudMap.jl
+++ src/services/PlotPointCloudMap.jl
@@ -67,7 +67,8 @@ function plotGraphPointClouds(
   dfg::AbstractDFG,
   getpointcloud::Function = (v)->_PCL.getDataPointCloud(dfg, v, Regex("PCLPointCloud2"); checkhash=false);
   varList = (listVariables(dfg) |> sortDFG .|> string),
-  solveKey = :default
+  solveKey = :default,
+  ax=nothing
 )
   pl = nothing
 
@@ -89,6 +90,7 @@ function plotGraphPointClouds(
   M = getManifold(Pose3)
   ϵ0_ = MJL.identity_element(M)
   ϵ0 = ArrayPartition(ϵ0_.parts...)
+  pc_map = _PCL.PointCloud()
   for vl in varList
     @show vl
     count += 1
@@ -114,21 +116,23 @@ function plotGraphPointClouds(
     wPp = MJL.exp(M,ϵ0,MJL.hat(M,ϵ0,w_Cwp))
     # wPp = getSolverData(v, solveKey).val[1]
     wPC = Caesar._PCL.apply(M, wPp, pc)
-
-    if pl isa Nothing
-      pl = plotPointCloud(wPC; plotfnc = scatter, col=-1.0, ax=nothing)
-      pc_map = deepcopy(wPC)
+    
+    
+    if isnothing(pl) && isnothing(ax)
+      pl = plotPointCloud(wPC; plotfnc = scatter, col=-1.0, ax)
+      # pc_map = deepcopy(wPC)
     else
       # col = -1*(count%2)
       col = if vl == varList[end]
         pc_last = wPC
         0
       else
-        pc_map = cat(pc_map, wPC; reuse=true)
+        # pc_map = cat(pc_map, wPC; reuse=true)
         -1
       end
-      plotPointCloud(wPC; plotfnc = scatter!) #, col)
+      plotPointCloud(wPC; plotfnc = scatter!, ax) #, col)
     end
+    cat(pc_map, wPC; reuse=true)
   end
 
   return pl, pc_map, pc_last
@@ -137,11 +141,11 @@ end
 
 function plotBoundingBox!(
   ax,
-  BB::_PCL.AbstractBoundingBox,
+  BB::_PCL.AbstractBoundingBox;
   color=:red
 )
-  _line3d!(ax, a, b) = lines!(ax, [b[1];a[1]], [b[2];a[2]], [b[3];a[3]]; color  )
-
+  _line3d!(ax, a, b; _color=color ) = lines!(ax, [b[1];a[1]], [b[2];a[2]], [b[3];a[3]]; color=_color  )
+  
   corners = _PCL.getCorners(BB)
   
   # https://math.stackexchange.com/questions/1472049/check-if-a-point-is-inside-a-rectangular-shaped-area-3d
@@ -149,17 +153,21 @@ function plotBoundingBox!(
   _line3d!(ax, corners[1], corners[4])
   _line3d!(ax, corners[2], corners[3])
   _line3d!(ax, corners[4], corners[3])
-
+  
   _line3d!(ax, corners[5], corners[6])
   _line3d!(ax, corners[5], corners[8])
   _line3d!(ax, corners[6], corners[7])
   _line3d!(ax, corners[8], corners[7])
-
+  
   _line3d!(ax, corners[1], corners[5])
   _line3d!(ax, corners[2], corners[6])
   _line3d!(ax, corners[3], corners[7])
   _line3d!(ax, corners[4], corners[8])
-
+  
+  if BB isa _PCL.OrientedBoundingBox
+    _line3d!(ax, [0;0;0.], corners[1]; _color=:gray)
+  end
+  
   nothing
 end
 
