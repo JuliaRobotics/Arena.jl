907764b0e498e2ed16ae389196b4d986df0a5ac2
dehann <dehann.fourie@gmail.com>
2023-11-21 04:27:16 -0800

    wip towards histBelief

--- src/services/PlotHistogramGrid.jl
+++ src/services/PlotHistogramGrid.jl
@@ -1,6 +1,9 @@
 # plotting tools for fields in factor graphs
 
 
+_getBeliefRange(s::ManifoldKernelDensity; extend=0.1) = getKDERange(s;extend)
+_getBeliefRange(s::MvNormal; extend=0.1) = [(s.μ[1]-3*s.Σ[1,1]) (s.μ[1]+3*s.Σ[1,1]); (s.μ[2]-3*s.Σ[2,2]) (s.μ[2]+3*s.Σ[2,2])]
+
 
 """
     $SIGNATURES
@@ -14,57 +17,77 @@ DevNotes
 - TODO, allow `tags` as filter too.
 """
 function getRangeCartesian(
+  P::Union{<:ManifoldKernelDensity,<:MvNormal};
+  xmin::Real=99999999,
+  xmax::Real=-99999999,
+  ymin::Real=99999999,
+  ymax::Real=-99999999,
+  extend::Float64=0.2,
+  digits::Int=6,
+  force::Bool=false,
+)
+  if force
+    return [xmin xmax; ymin ymax]
+  end
+
+  lran = _getBeliefRange(P)
+  xmin = lran[1,1] < xmin ? lran[1,1] : xmin
+  ymin = lran[2,1] < ymin ? lran[2,1] : ymin
+  xmax = xmax < lran[1,2] ? lran[1,2] : xmax
+  ymax = ymax < lran[2,2] ? lran[2,2] : ymax
+
+  # extend the range for looser bounds on plot
+  xra = xmax-xmin; xra *= extend
+  yra = ymax-ymin; yra *= extend
+  xmin -= extend; xmax += extend
+  ymin -= extend; ymax += extend
+
+  # clamp to nearest digits
+  xmin = floor(xmin, digits=digits); xmax = ceil(xmax, digits=digits)
+  ymin = floor(ymin, digits=digits); ymax = ceil(ymax, digits=digits)
+
+  return [xmin xmax; ymin ymax]
+end
+
+function getRangeCartesian(
   dfg::AbstractDFG,
   regexFilter::Union{Nothing, Regex}=nothing;
   varList = listVariables(dfg, regexFilter),
   factorList = Symbol[],
-  extend::Float64=0.2,
-  digits::Int=6,
   xmin::Real=99999999,
   xmax::Real=-99999999,
   ymin::Real=99999999,
   ymax::Real=-99999999,
   force::Bool=false,
+  kw...
 )
-  #
+  if force
+    return [xmin xmax; ymin ymax]
+  end
 
-  if !force
-    # which variables to consider
+  # which variables to consider
+  # find the cartesian range over all the varList variables
+  for vsym in varList
+    lran = getRangeCartesian(getVariable(dfg, vsym) |> getBelief; force, xmin,xmax,ymin,ymax,kw...)
+    xmin = lran[1,1] < xmin ? lran[1,1] : xmin
+    ymin = lran[2,1] < ymin ? lran[2,1] : ymin
+    xmax = xmax < lran[1,2] ? lran[1,2] : xmax
+    ymax = ymax < lran[2,2] ? lran[2,2] : ymax
+  end
 
-    # find the cartesian range over all the varList variables
-    for vsym in varList
-      lran = getKDERange(getVariable(dfg, vsym) |> getBelief)
-      xmin = lran[1,1] < xmin ? lran[1,1] : xmin
-      ymin = lran[2,1] < ymin ? lran[2,1] : ymin
-      xmax = xmax < lran[1,2] ? lran[1,2] : xmax
-      ymax = ymax < lran[2,2] ? lran[2,2] : ymax
-    end
-
-    _getBeliefRange(s::ManifoldKernelDensity;extend=0.1) = getKDERange(s;extend)
-    _getBeliefRange(s::MvNormal;extend=0.1) = [(s.μ[1]-3*s.Σ[1,1]) (s.μ[1]+3*s.Σ[1,1]); (s.μ[2]-3*s.Σ[2,2]) (s.μ[2]+3*s.Σ[2,2])]
-
-    # FIXME only works for very limited set of factors, has .Z and complies with Position{2}
-    for fsym in factorList
-      lran = _getBeliefRange(getFactorType(dfg, fsym).Z)
-      xmin = lran[1,1] < xmin ? lran[1,1] : xmin
-      ymin = lran[2,1] < ymin ? lran[2,1] : ymin
-      xmax = xmax < lran[1,2] ? lran[1,2] : xmax
-      ymax = ymax < lran[2,2] ? lran[2,2] : ymax
-    end
-
-    # extend the range for looser bounds on plot
-    xra = xmax-xmin; xra *= extend
-    yra = ymax-ymin; yra *= extend
-    xmin -= extend; xmax += extend
-    ymin -= extend; ymax += extend
-
-    # clamp to nearest integers
-    xmin = floor(xmin, digits=digits); xmax = ceil(xmax, digits=digits)
-    ymin = floor(ymin, digits=digits); ymax = ceil(ymax, digits=digits)
+  # FIXME only works for very limited set of factors, has .Z and complies with Position{2}
+  for fsym in factorList
+    lran = getRangeCartesian(getFactorType(dfg, fsym).Z; force, xmin,xmax,ymin,ymax,kw...)
+    xmin = lran[1,1] < xmin ? lran[1,1] : xmin
+    ymin = lran[2,1] < ymin ? lran[2,1] : ymin
+    xmax = xmax < lran[1,2] ? lran[1,2] : xmax
+    ymax = ymax < lran[2,2] ? lran[2,2] : ymax
   end
 
   return [xmin xmax; ymin ymax]
 end
+
+
 
 function getRange(
   dfg::AbstractDFG,
@@ -84,12 +107,52 @@ function getRange(
   coords[:ymax] = axes[2,2]
   if 0 < length(varList)
     if typeof(getVariableType(dfg, varList[1])).name.name in [:Pose3; :Position3; :RotVelPos]
-      coords[:zmin] = 9e10
-      coords[:zmax] = -9e10
+      coords[:zmin] = 99999999
+      coords[:zmax] = -99999999
     end
   end
 
   return coords
+end
+
+
+function _makeDens2D(_P::ManifoldKernelDensity)
+  P_ = marginal(_P,[1;2])
+  P__ = manikde!(
+    Position2, #getManifold(P_),
+    getPoints(P_,true),
+    bw=getBW(P_)
+  )
+  P__
+end
+function _makeDens2D(_P::MvNormal)
+  s->pdf(MvNormal(_P.μ[1:2], _P.Σ[1:2,1:2]),s[:])
+end
+
+function histBelief2D!(
+  img::AbstractMatrix, 
+  i::Integer, 
+  x::Real, 
+  j::Integer, 
+  y::Real, 
+  P
+)
+  P_ = _makeDens2D(P)
+  roi = _getBeliefRange(P_; extend=0.3)
+  if (roi[1,1] <= x <= roi[1,2]) && (roi[2,1] <= y <= roi[2,2])
+    ev = zeros(2,1)
+    ev[1,1] = x
+    ev[2,1] = y
+    img[i,j] += P_(ev)[1]
+  end
+  nothing
+end
+
+function histBelief2D(
+  P::ManifoldKernelDensity
+)
+  #
+
 end
 
 
@@ -104,34 +167,18 @@ function histGrid(
   coords = getRange(dfg; varList, factorList)
 
   img = zeros(N,N)
-  
-  ev = zeros(2,1)
-
-  _makeDens2D(_P::ManifoldKernelDensity) = begin
-    P_ = marginal(_P,[1;2])
-    P__ = manikde!(
-      Position2, #getManifold(P_),
-      getPoints(P_,true),
-      bw=getBW(P_)
-    )
-    P__
-  end
-  _makeDens2D(_P::MvNormal) = begin
-    s->pdf(MvNormal(_P.μ[1:2], _P.Σ[1:2,1:2]),s[:])
-  end
-  _getBeliefRange(s::ManifoldKernelDensity; extend=0.1) = getKDERange(s;extend)
-  _getBeliefRange(s::MvNormal; extend=0.1) = [(s.μ[1]-3*s.Σ[1,1]) (s.μ[1]+3*s.Σ[1,1]); (s.μ[2]-3*s.Σ[2,2]) (s.μ[2]+3*s.Σ[2,2])]
 
   _accumulateDens!(img, i, x, j, y, _v) = begin
     P = getBelief(getVariable(dfg,_v))
-    P_ = _makeDens2D(P)
-    roi = _getBeliefRange(P_; extend=0.3)
-    if (roi[1,1] <= x <= roi[1,2]) && (roi[2,1] <= y <= roi[2,2])
-      ev[1,1] = x
-      ev[2,1] = y
-      img[i,j] += P_(ev)[1]
-    end
-    nothing
+    histBelief2D!(img,i,x,j,y,P)
+    # P_ = _makeDens2D(P)
+    # roi = _getBeliefRange(P_; extend=0.3)
+    # if (roi[1,1] <= x <= roi[1,2]) && (roi[2,1] <= y <= roi[2,2])
+    #   ev[1,1] = x
+    #   ev[2,1] = y
+    #   img[i,j] += P_(ev)[1]
+    # end
+    # nothing
   end
 
   NNv = N*N*length(varList)
