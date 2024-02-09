cd0b9162ce45a7dd42caadd9c485f408c6a54999
Johannes Terblanche <Affie@users.noreply.github.com>
2024-02-09 10:43:18 +0200

    rm ImageMagick

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 # workaround is to load ImageMagick.jl first, front and center
 # see https://github.com/JuliaIO/ImageMagick.jl/issues/142
 # and https://github.com/JuliaIO/ImageMagick.jl/issues/130
-using ImageMagick
+# using ImageMagick
 
 using Pkg
 
