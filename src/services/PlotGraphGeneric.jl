2cea1f6c7bf43788d42ae540ee2487d464f86c7f
dehann <dehann.fourie@gmail.com>
2024-08-12 23:45:56 -0700

    impv plot3d!

--- src/services/PlotGraphGeneric.jl
+++ src/services/PlotGraphGeneric.jl
@@ -17,9 +17,10 @@ function plot3d!(
   drawTrajectory::Bool=true,
   drawTrajectoryMarkers::Bool = drawTrajectory,
   drawTriads::Bool = false,
-  linewidth = 0.025,
+  linewidth = 0.05,
   lengthscale=0.15f0,
   arrowsize = Vec3f(0.05, 0.05, 0.1),
+  aspect = :data,
 )
   #
 
@@ -30,12 +31,7 @@ function plot3d!(
   _getppepos(_v::DFGVariable{<:RoME.VelPos3}, solvK) = getPPESuggested(_v, solvK)[4:6]
   _getppepos(_v::DFGVariable{<:RoME.RotVelPos}, solvK) = getPPESuggested(_v, solvK)[7:9]
 
-  ax1 = Axis3(fig[1, 1]; title)
-  # Axis(f[2, 1], title = L"\sum_i{x_i \times y_i}")
-  # Axis(f[3, 1], title = rich(
-  #     "Rich text title",
-  #     subscript(" with subscript", color = :slategray)
-  # ))
+  ax1 = Axis3(fig[1, 1]; title, aspect)
   
   _ppes = _getppepos.(getVariable.(dfg, labels), solveKey) # getPPESuggested.(dfg, labels, solveKey)
 
@@ -52,9 +48,16 @@ function plot3d!(
     axislegend(ax1)
   end
 
+  _getPos(::RotVelPos, p::ArrayPartition) = p.x[3]
+  _getPos(::Pose2, p::ArrayPartition) = [p.x[1]...; 0.0]
+  _getPos(::Pose3, p::ArrayPartition) = p.x[1]
+  _getPos(::Position{1}, p::AbstractVector) = [p[1]; 0; 0.0]
+  _getPos(::Position{2}, p::AbstractVector) = [p[1]; p[2]; 0.0]
+  _getPos(::Position{N} where N, p::AbstractVector) = p[1:3]
+  
   _getRot(::RotVelPos, p::ArrayPartition) = p.x[1]
-  _getRot(::Pose2, p::ArrayPartition) = p.x[2]
-  _getRot(::Pose3, p::ArrayPartition) = [p.x[2][1,1] p.x[2][1,2] 0; p.x[2][2,1] p.x[2][2,2] 0; 0 0 1.0] 
+  _getRot(::Pose3, p::ArrayPartition) = p.x[2]
+  _getRot(::Pose2, p::ArrayPartition) = [p.x[2][1,1] p.x[2][1,2] 0; p.x[2][2,1] p.x[2][2,2] 0; 0 0 1.0] 
   _getRot(::Position{N} where N, p::ArrayPartition) = diagm(ones(3))
   
   pos = []
@@ -65,14 +68,14 @@ function plot3d!(
     # NOTE using val (not PPE), this will help show when PPEs are out of step with val
     for var in getVariable.(dfg, labels)
       ps = calcMean(getBelief(var, solveKey))
-      push!(pos, Point3f(ps.x[3]))
-      rot = _getRot(var,ps)
-      push!(nxs, Point3f(rot[:,1]))
-      push!(nys, Point3f(rot[:,2]))
-      push!(nzs, Point3f(rot[:,3]))
+      push!(pos, Point3f(_getPos(getVariableType(var), ps)))
+      rot = _getRot(getVariableType(var),ps)
+      push!(nxs, Point3f(rot[:,1]...))
+      push!(nys, Point3f(rot[:,2]...))
+      push!(nzs, Point3f(rot[:,3]...))
       Makie.arrows!(ax1, pos, nxs; color=:red, linewidth, lengthscale, arrowsize)
       Makie.arrows!(ax1, pos, nys; color=:green, linewidth, lengthscale, arrowsize)
-      # Makie.arrows!(ax1, pos, nzs; color=:blue , linewidth, lengthscale, arrowsize)
+      Makie.arrows!(ax1, pos, nzs; color=:blue , linewidth, lengthscale, arrowsize)
     end
   end
 
